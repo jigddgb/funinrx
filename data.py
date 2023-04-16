2524d3ca1d6d6571d477037b4fe8c0a4f678f909
jigddgb <124061396+jigddgb@users.noreply.github.com>
2023-04-16 12:36:08 +0600

    Update data.py

--- data.py
+++ data.py
@@ -8,8 +8,8 @@ class Data:
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("❣️ sᴜᴩᴩᴏʀᴛ ❣️", url="https://t.me/Its_venom_family"),
-         InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url="https://t.me/Venom_bolte_public"),
+        [InlineKeyboardButton("❣️ sᴜᴩᴩᴏʀᴛ ❣️", url="https://t.me/TheNight_City"),
+         InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", url="https://t.me/Bothub_xD"),
         ],
     ]
 
@@ -19,6 +19,6 @@ Hᴇʏ {},
 Tʜɪs ɪs {},
 Aɴ ᴏᴩᴇɴ sᴏᴜʀᴄᴇᴅ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
 
-Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://github.com/ItzvenomXd/StringGenBot)
-Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [ᴍʀ ᴀʏᴜsʜ](https://t.me/Venom_bolte_public) !
+Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://t.me//TheNight_City)
+Mᴀᴅᴇ ᴡɪᴛʜ 🖤 ʙʏ : [ᴍʀ ᴀʏᴜsʜ](https://t.me/Bothub_xD) !
     """
