8ce0d63820dc503e06122045ef57c63436ab94ef
kyakregajnkevai <124061396+jigddgb@users.noreply.github.com>
2023-12-31 18:58:29 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,14 +2,14 @@ from pyrogram.types import InlineKeyboardButton
 
 
 class Data:
-    generate_single_button = [InlineKeyboardButton("🙄 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🙄", callback_data="generate")]
+    generate_single_button = [InlineKeyboardButton("✘ ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ ✘", callback_data="generate")]
 
     generate_button = [generate_single_button]
 
     buttons = [
         generate_single_button,
-        [InlineKeyboardButton("🌃 ɴɪɢʜᴛ_ᴄɪᴛʏ 🌃", url="https://t.me/TheNight_City"),
-         InlineKeyboardButton("↳ ᴅᴇᴠᴇʟᴏᴩᴇʀ ↵", url="https://t.me/Bothub_xD"),
+        [InlineKeyboardButton("🌃 Bᴏᴛʜᴜʙ_xD 🌃", url="https://t.me/Bothub_XD"),
+         InlineKeyboardButton("↳ ᴅᴇᴠᴇʟᴏᴩᴇʀ ↵", url="https://t.me/NotSuppie"),
         ],
     ]
 
@@ -19,6 +19,6 @@ Hᴇʏ {},
 Tʜɪs ɪs {},
 Aɴ ᴏᴩᴇɴ sᴏᴜʀᴄᴇᴅ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
 
-Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://t.me//TheNight_City)
-ᴅᴇᴠᴇʟᴏᴘᴇʀ : [ꜱᴜᴘᴘɪᴇ](https://t.me/SuppieNoodles) !
+Sᴏᴜʀᴄᴇ : [ɢɪᴛʜᴜʙ](https://t.me//Bothub_XD)
+ᴅᴇᴠᴇʟᴏᴘᴇʀ : [ꜱᴜᴘᴘɪᴇ](https://t.me/NotSuppie) !
     """
